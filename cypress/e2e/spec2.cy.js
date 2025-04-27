5efba169d660a8f85f8cdee79ecf41c265d27d0b
Caio Haendel <caiohaendel.pessoal@outlook.com>
2025-04-27 10:14:01 -0300

    refatoração e teste de busca - aula 05

--- cypress/e2e/spec2.cy.js
+++ cypress/e2e/spec2.cy.js
@@ -1,10 +1,30 @@
 /// <reference types="cypress"/>
 
 describe('US-012-Funcionalidade: Buscar filmes', () => {
-  it('Deve buscar pelo filme digitado', () => {
-    cy.visit('http://192.168.18.60:8080/')
-    cy.get('#search-input').click()
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('Deve buscar filmes com sucesso', () => {
     cy.get('#search-input').type('Batman')
     cy.get('#search-button').click()
+    cy.get('#results-section').should('contain', 'Batman')
   })
+
+  it('Deve buscar filmes com sucesso de uma lista', () => {
+    cy.fixture('filmes').then((filmes) => {
+      cy.get('#search-input').type(filmes[1].titulo)
+      cy.get('#search-button').click()
+      cy.get('#results-section').should('contain', filmes[1].titulo)
+    })
+  })
+
+  it.only('Deve buscar filmes com sucesso de uma lista interia', () => {
+    cy.fixture('filmes').each((filmes) => {
+      cy.get('#search-input').clear().type(filmes.titulo)
+      cy.get('#search-button').click({force: true})
+      cy.get('#results-section').should('contain', filmes.titulo)
+    })
+  })
+
 })
